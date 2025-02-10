95239b9e6716d751b7c9191cd92803556e1aecdf
jungyouna12 <jungyouna12@naver.com>
2025-02-10 19:40:50 +0900

    [신나리] - 프로젝트 생성

---
+++ src/test/java/com/shinnal/spring/ex/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinnal.spring.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
